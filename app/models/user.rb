51bad271e0f946452ccb70e6c9df59b1a7041bdb
Amier Ariffin <muhdamierrosli@hotmail.com>
2017-01-23 15:07:05 +0800

    generated view pages and controllers for items and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+
+has_secure_password
+
+validates :username, presence: true
+validates :email, presence: true
+
+
+has_many :items
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+resources :users
+resources :items
+
+root 'items#index'
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
